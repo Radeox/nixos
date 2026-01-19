bf6f0f0800fcb8d8f08e88265a1a22b871e04085
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-01-19 10:04:18 +0100

    Update some XDG apps

--- home-manager/environment.nix
+++ home-manager/environment.nix
@@ -1,13 +1,21 @@
-{pkgs,...}:
+{ pkgs, ... }:
 {
   xdg.mimeApps = {
     enable = true;
     defaultApplications = {
+      # HTML and Browser
       "text/html" = [ "app.zen_browser.zen.desktop" ];
       "x-scheme-handler/http" = [ "app.zen_browser.zen.desktop" ];
       "x-scheme-handler/https" = [ "app.zen_browser.zen.desktop" ];
       "x-scheme-handler/about" = [ "app.zen_browser.zen.desktop" ];
       "x-scheme-handler/unknown" = [ "app.zen_browser.zen.desktop" ];
+
+      # PDF
+      "application/pdf" = [ "app.zen_browser.zen.desktop" ];
+
+      # Text files
+      "text/plain" = [ "org.gnome.TextEditor.desktop" ];
+      "text/markdown" = [ "org.gnome.TextEditor.desktop" ];
     };
   };
 
