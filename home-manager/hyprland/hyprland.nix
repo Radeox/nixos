04dcf7cd805fd930c11482e1390513cee16e1cba
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-02-26 17:31:19 +0100

    Update hyprland window animation

--- home-manager/hyprland/hyprland.nix
+++ home-manager/hyprland/hyprland.nix
@@ -63,15 +63,22 @@
       animations = {
         enabled = true;
 
-        bezier = "slideCurve, 0.05, 0.9, 0.1, 1.0";
+        bezier = [
+          "wind, 0.05, 0.9, 0.1, 1.05"
+          "winIn, 0.1, 1.1, 0.1, 1.1"
+          "winOut, 0.3, -0.3, 0, 1"
+          "liner, 1, 1, 1, 1"
+        ];
 
         animation = [
-          "windows, 1, 7, slideCurve"
-          "windowsOut, 1, 7, default, slide right"
-          "border, 1, 10, default"
-          "borderangle, 1, 8, default"
-          "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
+          "windows, 1, 6, wind, slide"
+          "windowsIn, 1, 6, winIn, slide"
+          "windowsOut, 1, 5, winOut, slide"
+          "windowsMove, 1, 5, wind, slide"
+          "border, 1, 1, liner"
+          "borderangle, 1, 30, liner, loop"
+          "fade, 1, 10, default"
+          "workspaces, 1, 5, wind, slide"
         ];
       };
 
