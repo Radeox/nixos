c0507788d0aae6dc6fa15d9ded15cb40f0bd70fa
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-05-04 11:12:55 +0200

    Add width cycle keybind

--- home-manager/niri/niri.nix
+++ home-manager/niri/niri.nix
@@ -28,6 +28,7 @@
       layout = {
         gaps = 12;
         center-focused-column = "never";
+
         preset-column-widths = [
           { proportion = 1.0 / 3.0; }
           { proportion = 1.0 / 2.0; }
@@ -159,24 +160,12 @@
         "Mod+2".action.focus-workspace = 2;
         "Mod+3".action.focus-workspace = 3;
         "Mod+4".action.focus-workspace = 4;
-        "Mod+5".action.focus-workspace = 5;
-        "Mod+6".action.focus-workspace = 6;
-        "Mod+7".action.focus-workspace = 7;
-        "Mod+8".action.focus-workspace = 8;
-        "Mod+9".action.focus-workspace = 9;
-        "Mod+0".action.focus-workspace = 10;
 
         # Move active window to a workspace
         "Mod+Shift+1".action.move-column-to-workspace = 1;
         "Mod+Shift+2".action.move-column-to-workspace = 2;
         "Mod+Shift+3".action.move-column-to-workspace = 3;
         "Mod+Shift+4".action.move-column-to-workspace = 4;
-        "Mod+Shift+5".action.move-column-to-workspace = 5;
-        "Mod+Shift+6".action.move-column-to-workspace = 6;
-        "Mod+Shift+7".action.move-column-to-workspace = 7;
-        "Mod+Shift+8".action.move-column-to-workspace = 8;
-        "Mod+Shift+9".action.move-column-to-workspace = 9;
-        "Mod+Shift+0".action.move-column-to-workspace = 10;
 
         # Scroll workspaces
         "Mod+WheelScrollDown".action.focus-workspace-down = { };
@@ -187,6 +176,9 @@
 
         # Switch keyboard layout
         "Alt+Space".action.switch-layout = "next";
+
+        # Cycle between width presets
+        "Mod+R".action.switch-preset-window-width = { };
 
         # Power menu (Noctalia)
         "Mod+Shift+E".action.spawn = [
@@ -240,6 +232,7 @@
             bottom-left = 12.0;
             bottom-right = 12.0;
           };
+
           clip-to-geometry = true;
         }
         {
