296ad26167a4ffb386ee57d7b105fd4c69a647f8
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-05-07 15:45:19 +0200

    Merge hardware configs

--- hardware/default.nix
+++ hardware/default.nix
@@ -1,7 +1,53 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  imports = [
-    ./audio.nix
-    ./hardware.nix
-  ];
+  hardware = {
+    # Enable graphics drivers
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+
+    # Enable bluetooth
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+
+      settings = {
+        General = {
+          # Show battery levels for connected devices
+          Experimental = true;
+        };
+
+        Policy = {
+          # Enable all controllers when they are found
+          AutoEnable = true;
+        };
+      };
+    };
+
+    # Enable support for SANE scanners
+    sane = {
+      enable = true;
+      extraBackends = [ pkgs.sane-airscan ];
+    };
+
+    # Enable the Xbox One controller driver
+    xone.enable = true;
+  };
+
+  services = {
+    # Pipewire configuration
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    # Disable pulseaudio
+    pulseaudio.enable = false;
+  };
+
+  # For pipewire
+  security.rtkit.enable = true;
 }
